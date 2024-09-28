cc7b630c84c9462a9f053aaf759f9c7b7f4031cd
Fatih Kara <fkara1667@gmail.com>
2024-09-28 15:00:27 +0300

    deposit added

---
+++ Lesson_01/Models/Account.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_01.Models
+{
+    class Account
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string IBAN { get; set; }
+        private decimal Balance { get; set; }
+
+        public Account(decimal balance)
+        {
+            Balance = balance;
+        }
+
+        public Account()
+        {
+            Balance = 100;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            else if (Balance != 0)
+            {
+                System.Console.WriteLine($"Insufficient balance but you withdraw this amount: {Balance}");
+                Balance = 0;
+            }
+            else
+            {
+                System.Console.WriteLine("Insufficient balance");
+            }
+        }
+
+        public void SetBalance(decimal balance)
+        {
+            if (Balance <= 1000)
+                Balance = balance;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public decimal GetBalance() //Geri dönüş tipi Decimal olan bir metot ile değiştirilecek
+        {
+            return Balance;
+        }
+    }
+}
--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -1,61 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Lesson_01.Models;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Bank App");
+
+List<Account> accounts = new();
 
 Account account1 = new Account(1000);
 account1.FirstName = "John";
 account1.LastName = "Black";
 account1.PhoneNumber = "1234567890";
 account1.IBAN = "TR123654555";
-//account1.Balance = 1000;
-//account1.SetBalance(1000);
 
-account1.Withdraw(1500);
-account1.Withdraw(550);
+Account account2 = new();
+account2.FirstName = "Jane";
+account2.LastName = "Doe";
+account2.PhoneNumber = "987654321";
+account2.IBAN = "TR987654321";
+accounts.Add(account2);
 
-Console.WriteLine(account1.GetBalance());
+string depositIBAN = "TR987654321";
+decimal depositAmount = 750;
 
-//Console.WriteLine(account1.Balance);
-
-
-class Account
+for (int i = 0; i < accounts.Count; i++)
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string IBAN { get; set; }
-    private decimal Balance { get; set; }
-
-    public Account(decimal balance)
+    if (accounts[i].IBAN == depositIBAN)
     {
-        Balance = balance;
-    }
-
-    public void Withdraw(decimal amount)
-    {
-        if (Balance >= amount)
-        {
-            Balance -= amount;
-        }
-        else if (Balance != 0)
-        {
-            System.Console.WriteLine($"Insufficient balance but you withdraw this amount: {Balance}");
-            Balance = 0;
-        }
-        else
-        {
-            System.Console.WriteLine("Insufficient balance");
-        }
-    }
-
-    public void SetBalance(decimal balance)
-    {
-        if (Balance <= 1000)
-            Balance = balance;
-    }
-
-    public decimal GetBalance() //Geri dönüş tipi Decimal olan bir metot ile değiştirilecek
-    {
-        return Balance;
+        accounts[i].Deposit(depositAmount);
     }
 }
+
+Console.WriteLine($"John's Balance: {account1.GetBalance()}");
+Console.WriteLine($"Jane's Balance: {account2.GetBalance()}");
+
+//account1.Withdraw(1500);
+//account1.Withdraw(550);
+
+
+
 
