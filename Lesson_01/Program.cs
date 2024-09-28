e7d30b442453e846c7baaecae154e1d75e3fb140
Fatih Kara <fkara1667@gmail.com>
2024-09-28 14:46:00 +0300

    Withdraw method updated

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -1,13 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Bank App");
 
-Account account1 = new Account();
+Account account1 = new Account(1000);
 account1.FirstName = "John";
 account1.LastName = "Black";
 account1.PhoneNumber = "1234567890";
 account1.IBAN = "TR123654555";
 //account1.Balance = 1000;
-account1.SetBalance(1000);
+//account1.SetBalance(1000);
 
 account1.Withdraw(1500);
 account1.Withdraw(550);
@@ -25,11 +25,25 @@ class Account
     public string IBAN { get; set; }
     private decimal Balance { get; set; }
 
+    public Account(decimal balance)
+    {
+        Balance = balance;
+    }
+
     public void Withdraw(decimal amount)
     {
         if (Balance >= amount)
         {
             Balance -= amount;
+        }
+        else if (Balance != 0)
+        {
+            System.Console.WriteLine($"Insufficient balance but you withdraw this amount: {Balance}");
+            Balance = 0;
+        }
+        else
+        {
+            System.Console.WriteLine("Insufficient balance");
         }
     }
 
